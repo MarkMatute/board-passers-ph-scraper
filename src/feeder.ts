c3e5004c23d0ec0b22a4629ef3bef2b7a4867c2a
Mark Matute <mark.ernest@healthbeats.co>
2019-10-16 08:03:05 +0800

    Feeder Wip

--- src/feeder.ts
+++ src/feeder.ts
@@ -1,24 +1,45 @@
 import fs from 'fs';
+import axios from 'axios';
 
 class Feeder {
 
   apiUrl: string = 'http::/localhost'
 
   public startFeed = async () => {
-    const jsonFiles = await this.getJsonFiles();
-    console.log(jsonFiles)
+    const outputs = await this.getJsonFiles();
+     await this.sendToServer(outputs);
   }
 
   private getJsonFiles = async () => {
-   return new Promise((resolve, reject) => {
+    return new Promise((resolve, reject) => {
       fs.readdir('./outputs', (err, files) => {
-        const fileNames: string[] = [];
+        const output: any[] = [];
         files.forEach((file) => {
-          fileNames.push(file);
+          fs.readFile(`./outputs/${file}`, (err, data) => {
+            if (err) throw err;
+            try {
+              let parsedData = JSON.parse(data.toString());
+              output.push(parsedData);
+            } catch (error) {
+              console.log(`${file} failed.`);
+            }
+          });
         });
-        resolve(fileNames);
+        resolve(output);
       });
-   });
+    });
+  }
+
+  private sendToServer = async (outputs: any) => {
+
+  }
+
+  private saveExam = async () => {
+
+  }
+
+  private savePassers = async () => {
+    
   }
 
 }
